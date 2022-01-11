052f137e1a52cde378d85233daf9b629ea2771b1
Marcos Hernanz <96699542+MarcosH911@users.noreply.github.com>
2022-01-11 22:17:05 +0100

    Create Depth-First-Search.py

---
+++ Algorithms-and-Data-Structures/Data-Structures-Implementations/Graphs/Depth-First-Search.py
@@ -0,0 +1,29 @@
+def iterative_dfs(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return sorted(visited)
+
+
+def recursive_dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for nxt in graph[start] - visited:
+        recursive_dfs(graph, nxt, visited)
+    return sorted(visited)
+
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for nxt in graph[vertex] - set(path):
+            if nxt == goal:
+                yield path + [nxt]
+            else:
+                stack.append((nxt, path + [nxt]))
