10a940b559ad5791459be887f549128c783f3562
Marcos Hernanz <96699542+MarcosH911@users.noreply.github.com>
2022-01-13 20:00:40 +0100

    Create Breadth-First-Search.py

---
+++ Algorithms-and-Data-Structures/Graphs/Breadth-First-Search.py
@@ -0,0 +1,28 @@
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for nxt in graph[vertex] - set(path):
+            if nxt == goal:
+                yield path + [nxt]
+            else:
+                queue.append((nxt, path + [nxt]))
+
+
+def shortest_path_bfs(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+      
