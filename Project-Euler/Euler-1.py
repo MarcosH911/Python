2f8da8541b669fb5673a43bd544dd990ae98c758
Marcos Hernanz <96699542+MarcosH911@users.noreply.github.com>
2022-01-13 20:19:51 +0100

    Delete Euler-1.py

--- Project-Euler/Euler-1.py
+++
@@ -1,9 +0,0 @@
-def multiples_sum(num):
-    total_sum = 0
-    for i in range(1, num):
-        if i % 3 == 0 or i % 5 == 0:
-            total_sum += i
-    return total_sum
-
-
-print(multiples_sum(1000))
