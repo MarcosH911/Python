33a010d8aea3873ab6942edf0582b178428cddbb
Marcos Hernanz <96699542+MarcosH911@users.noreply.github.com>
2022-01-13 20:09:23 +0100

    Delete Algorithms-and-Data-Structures/Data-Structures-Implementations directory

--- Algorithms-and-Data-Structures/Data-Structures-Implementations/Searching-and-Sorting/Bubble-Sort.py
+++
@@ -1,6 +0,0 @@
-def bubble_sort(array):
-    for x in range(len(array) - 1, 0, -1):
-        for y in range(x):
-            if array[y] > array[y+1]:
-                array[y], array[y+1] = array[y+1], array[y]
-    return array
